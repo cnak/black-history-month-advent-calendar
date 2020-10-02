2b2a629861047e8bc031b0db2cd9bfb8d05951ef
Cedric Kisema <kisema@gmail.com>
2020-10-02 18:15:47 +0100

    innit commit

---
+++ src/confetti.js
@@ -0,0 +1,14 @@
+export const confettiConfig = {
+  angle: 90,
+  spread: 120,
+  startVelocity: 40,
+  elementCount: 70,
+  dragFriction: 0.12,
+  duration: 9000,
+  stagger: 3,
+  width: "10px",
+  height: "10px",
+  perspective: "714px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+};
+
