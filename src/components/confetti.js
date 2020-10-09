1a98e748457cf4d2541634375722c70fd3de0175
Cedric Kisema <kisema@gmail.com>
2020-10-09 16:56:38 +0100

    fixes confetti

--- src/components/confetti.js
+++ src/components/confetti.js
@@ -2,14 +2,14 @@ const confettiConfig = {
   angle: 90,
   spread: 50,
   startVelocity: 40,
-  elementCount: 70,
+  elementCount: 170,
   dragFriction: 0.12,
   duration: 2000,
   stagger: 3,
   width: '10px',
   height: '10px',
-  perspective: '500px',
-  colors: ['#a864fd', '#29cdff', '#78ff44', '#ff718d', '#fdff6a'],
+  perspective: '100%',
+  colors: ['#E55F13', '#647D1C', '#E8AA24']
 };
 
 export default confettiConfig;
