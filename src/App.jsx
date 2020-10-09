ce85ff528833ddcf649e5a4b52c3e68d53ff24bd
Cedric Kisema <kisema@gmail.com>
2020-10-09 16:55:42 +0100

    adds font

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,25 @@ import StyledApp from './AppStyles';
 import { createCalendar } from './helpers';
 import bhmBackground from './img/bhm.png';
 import Spinner from './components/Spinner'
+import PilatNarrowBlack from './fonts/PilatNarrow-Black.woff';
+import BEYNO from './fonts/BEYNO.woff';
 
 const Hatch = lazy(() => import ('./components/Hatch'));
 const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'BEYNO';
+    src: url(${BEYNO}) format('woff');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
+    @font-face {
+        font-family: 'Pilat Narrow';
+        src: url(${PilatNarrowBlack}) format('woff');
+        font-weight: 900;
+        font-style: normal;
+        font-display: swap;
+    }
   body {
     background-color: #1E1E1;
     margin: 0;
